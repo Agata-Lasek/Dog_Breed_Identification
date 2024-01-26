82d3fe8740f8d72b3541efe795949333532892cf
Agata-Lasek <264229@student.pwr.edu.pl>
2024-01-26 23:58:14 +0100

    Update Dog_Breed_Classification.py

--- Dog_Breed_Classification.py
+++ Dog_Breed_Classification.py
@@ -8,10 +8,11 @@ import random                               #do randomowych zdjec
 #from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.preprocessing import LabelEncoder
 from keras.utils import to_categorical 
+from sklearn.model_selection import train_test_split
 from keras.models import Sequential
-#from keras.layers import Conv2D, MaxPooling2D, Activation, Dropout, Flatten, Dense
+from keras.layers import Conv2D, MaxPooling2D, Activation, Dropout, Flatten, Dense
 #from keras import optimizers
-#from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing.image import ImageDataGenerator
 import warnings
 warnings.filterwarnings('ignore')
 
@@ -21,13 +22,16 @@ def main():
     
     # dane wejściowe
     data = pd.read_csv('C:/Users/agata/Desktop/dog-breed-identification/labels.csv')
+    test = pd.read_csv('C:/Users/agata/Desktop/dog-breed-identification/sample_submission.csv')
     print(data.head,"\n")                                                               #wyswietl kilka pierwszych linii
     print(data.shape,"\n")                                                              #tymczasowe - liczba danych
+    print(test.head,"\n")                                                               #wyswietl kilka pierwszych linii
+    print(test.shape,"\n")    
     print("Liczba unikalnych nazw ras psów: ",data["breed"].nunique(),"\n")  
     print("Liczba brakujących danych: \n",data.isna().sum(),"\n")                       #mamy wszystkie więc można spokojnie działać
     
     train = "C:/Users/agata/Desktop/train"                  # dane obrazy
-    #test="C:/Users/agata/Desktop/test"
+    test="C:/Users/agata/Desktop/test"
     
     # Liczba wierszy i kolumn w siatce - wizualizacja Train Data
     nrow = 5
@@ -44,8 +48,8 @@ def main():
         img = img.resize(target_size)                                                             # Dostosuj rozmiar obrazu do wspólnego rozmiaru
         dog_images.append(img)
         breeds.append(breed)
-        row = idx // ncol                                     # Oblicza aktualny wiersz
-        col = idx % ncol                                      # Oblicza aktualny kolumnę
+        row = idx // ncol                                   # Oblicza aktualny wiersz
+        col = idx % ncol                                    # Oblicza aktualny kolumnę
         
     dog_images_stack = np.stack(dog_images)                 # Przekształć listę obrazów, np.stack umożliwia łączenie sekwencji w nową oś
     breeds_stack = np.stack(breeds)
@@ -71,17 +75,64 @@ def main():
     
     
     # Przygotowanie danych do treningu modelu
-    X_train = dog_images_stack                          # zmienna, która przechowuje obrazy
-    y_train = encoded_breed                             # zmienna, która przechowuje przekształcone etykiety
+    X = dog_images_stack                          # zmienna, która przechowuje obrazy
+    y = encoded_breed                             # zmienna, która przechowuje przekształcone etykiety
     
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=30)      # oznacza, że 20% danych zostanie użyte jako zbiór testowy
+   
+
+    # Convolutional Neural Network
+    model = Sequential()        # Inicjalizacja modelu
+    
+    model.add(Conv2D(32, kernel_size=(3, 3), activation ='relu', input_shape = (224, 224, 3)))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    
+    model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    
+    model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    
+    model.add(Flatten())
+    
+    model.add(Dense(512, activation='relu'))
+    model.add(Dropout(0.5))
+    
+    model.add(Dense(num_breed, activation='softmax'))       # warstwa gęsta, num_breed określa liczbę neuronów w warstwie-  jest równoważne liczbie klas (120)
+    
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])  # Kompilacja modelu, optymalizator adam dostosowuje tempo uczenia się(najbardziej popularny), loss-funkcja straty określa, jak bardzo przewidywane wyjścia modelu różnią się od rzeczywistych etykiet
+    model.fit(X_train, y_train, batch_size, epochs=5)                                     # Trenowanie modelu dane treningowe zostaną przetworzone 5 razy przez cały model, batch_size=32 bo najbardziej popularny  
 
 
+    # Load and preprocess test images
+    dog_images_test = []
 
+#    for idx, image_id in enumerate(test["id"]):
+#        image_path = os.path.join(test_dir, f"{image_id}.jpg")
+#        img = load_img(image_path, target_size=target_size)
+#        img_array = img_to_array(img) / 255.0
+#        dog_images_test.append(img_array)
 
-    # Inicjalizacja modelu
-    model = Sequential()    
+#    dog_images_stack_test = np.stack(dog_images_test)
 
+    # Make predictions on test data
+#    predictions = model.predict(dog_images_stack_test)
 
+    # Decode predictions
+#    predicted_breeds_encoded = np.argmax(predictions, axis=1)
+#    predicted_breeds = LabelEncoder().fit(breeds).inverse_transform(predicted_breeds_encoded)
+
+    # Display 25 random test images with predicted breeds
+#    random_indices = np.random.choice(len(dog_images_stack_test), 25, replace=False)
+#    fig, ax = plt.subplots(5, 5, figsize=(10, 10))
+
+#    for i, idx in enumerate(random_indices):
+#        ax[i // 5, i % 5].imshow(dog_images_stack_test[idx])
+#        ax[i // 5, i % 5].set_title(predicted_breeds[idx])
+#        ax[i // 5, i % 5].axis("off")
+
+#    plt.tight_layout()
+#    plt.show()
 
 if __name__ == '__main__':
     main()
