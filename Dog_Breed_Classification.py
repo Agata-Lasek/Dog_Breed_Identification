eaad7e5a3ba60ceaf4562d9e91f54c5ef04fd67f
mkoper02 <maciej.koperdowski@gmail.com>
2024-01-27 14:46:04 +0100

    New display function and little code clean up

--- Dog_Breed_Classification.py
+++ Dog_Breed_Classification.py
@@ -1,10 +1,13 @@
 import numpy as np
 import pandas as pd
-from PIL import Image, ImageOps             #Wczytywanie obrazów bezpośrednio z plików
+from PIL import Image
 import matplotlib.pyplot as plt
 #import matplotlib.image as mpimg
-import os                                   # manipulacja  ścieżkam
-import random                               #do randomowych zdjec
+import os
+import random
+import warnings
+warnings.filterwarnings('ignore')
+
 #from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.preprocessing import LabelEncoder
 from keras.utils import to_categorical 
@@ -12,55 +15,60 @@ from keras.models import Sequential
 #from keras.layers import Conv2D, MaxPooling2D, Activation, Dropout, Flatten, Dense
 #from keras import optimizers
 #from keras.preprocessing.image import ImageDataGenerator
-import warnings
-warnings.filterwarnings('ignore')
 
 
+def displayInputData(dogs: list, breeds: list) -> None:
+    rows = 5
+    columns = 5
 
-def main():
-    
-    # dane wejściowe
-    data = pd.read_csv('C:/Users/agata/Desktop/dog-breed-identification/labels.csv')
-    print(data.head,"\n")                                                               #wyswietl kilka pierwszych linii
-    print(data.shape,"\n")                                                              #tymczasowe - liczba danych
-    print("Liczba unikalnych nazw ras psów: ",data["breed"].nunique(),"\n")  
-    print("Liczba brakujących danych: \n",data.isna().sum(),"\n")                       #mamy wszystkie więc można spokojnie działać
-    
-    train = "C:/Users/agata/Desktop/train"                  # dane obrazy
-    #test="C:/Users/agata/Desktop/test"
-    
-    # Liczba wierszy i kolumn w siatce - wizualizacja Train Data
-    nrow = 5
-    ncol = 5
-    fig,ax=plt.subplots(nrow,ncol,figsize=(10,10))                    # obiekt fig to cała figura, oraz tablica ax zawierającą podwykresy, z których każdy będzie używany do umieszczania jednego obrazu
-    
-    target_size=(224, 224)
-    dog_images = []                                                   # pusta lista do przechowywania obrazów psów
-    breeds = []                                                       # pusta lista do przechowywania ras psów
-    
-    for idx, (image_id, breed) in enumerate(data[["id", "breed"]].itertuples(index=False)):       # Iteracja po pierwszych nrow*ncol wierszach z ramki danych data
-        image_path = os.path.join(train, f"{image_id}.jpg")                                       # Ścieżka do obrazu
-        img = Image.open(image_path)                                                              # Wczytywanie obrazu za pomocą Pillow (PIL)
-        img = img.resize(target_size)                                                             # Dostosuj rozmiar obrazu do wspólnego rozmiaru
-        dog_images.append(img)
-        breeds.append(breed)
-        row = idx // ncol                                     # Oblicza aktualny wiersz
-        col = idx % ncol                                      # Oblicza aktualny kolumnę
-        
-    dog_images_stack = np.stack(dog_images)                 # Przekształć listę obrazów, np.stack umożliwia łączenie sekwencji w nową oś
-    breeds_stack = np.stack(breeds)
-    
-    # test wyswietlanie
-    for i in range(nrow * ncol):
-            row = i // ncol                                 # Oblicza aktualny wiersz
-            col = i % ncol                                  # Oblicza aktualny kolumnę
-            ax[row, col].imshow(dog_images_stack[i])        # Wyświetla obraz na odpowiednim podwykresie.
-            ax[row, col].set_title(breeds_stack[i])         # Dodaje tytuł podwykresu, który jest nazwą rasy psa   
-            ax[row, col].axis("off")                        # off oznaczenia osi, niekoniecznie - tylko estetyka
+    # obiekt fig to cała figura, oraz tablica ax zawierającą podwykresy, z których każdy będzie używany do umieszczania jednego obrazu
+    fig, ax=plt.subplots(rows, columns, figsize=(10,10))    
+
+    for row in range(rows):
+         for col in range(columns):
+            # Wyświetla obraz na odpowiednim podwykresie
+            ax[row, col].imshow(dogs[row * rows + col])  
+
+             # Dodaje tytuł podwykresu, który jest nazwą rasy psa       
+            ax[row, col].set_title(breeds[row * rows + col])  
+
+             # off oznaczenia osi, niekoniecznie - tylko estetyka   
+            ax[row, col].axis("off")                       
 
     plt.tight_layout()
-    plt.show()                                              # do wyświetlenia obrazu
+    plt.show()
+
+
+def main() -> None:
+    # dane wejściowe
+    data = pd.read_csv("data/labels.csv")
+
+    print(data.head(),"\n")
+    print(data.shape,"\n")
+
+    print("Liczba unikalnych nazw ras psów: ", data["breed"].nunique(),"\n")  
+            
+    dog_images = [] # pusta lista do przechowywania obrazów psów
+    breeds = []     # pusta lista do przechowywania ras psów
+
+    target_size = (224, 224)
     
+    for indx, (image_id, breed) in enumerate(data[["id", "breed"]].itertuples(index=False)):
+        # Ścieżka do obrazu
+        image_path = os.path.join("data/train", f"{image_id}.jpg")
+
+        # wczytaj rozmiar za pomoca pillow i dostosuj rozmiar obrazu
+        img = Image.open(image_path)
+        img = img.resize(target_size)
+
+        dog_images.append(img)
+        breeds.append(breed)
+
+    # Przekształć listę obrazów, np.stack umożliwia łączenie sekwencji w nową oś
+    dog_images_stack = np.stack(dog_images)                
+    breeds_stack = np.stack(breeds)
+
+    displayInputData(dog_images[:25], breeds[:25])   
     
     # Ustawienia
     num_breed = data["breed"].nunique()                 # Ustala docelową liczbę ras psów
@@ -80,7 +88,6 @@ def main():
 
     # Inicjalizacja modelu
     model = Sequential()    
-
 
 
 if __name__ == '__main__':
